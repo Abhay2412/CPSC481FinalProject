323f4328bea0fd9b674a4b17e559ce019b30bb36
Abhay2412 <abhaykhosla0@gmail.com>
2023-10-29 12:12:15 -0600

    Worked on creating the elements in the Welcome Screen

--- busticketingkiosk/src/screens/welcome.jsx
+++ busticketingkiosk/src/screens/welcome.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
-
-
+import { Button } from 'react-bootstrap';
+import BusWelcomeLogo from '../assets/images/BusWelcomeLogo.svg'
+import '../styles/Welcome.css'
 const WelcomeScreen = ({setPageTitle}) => {
 
     useEffect(() => {
-        setPageTitle("Welcome")
+        setPageTitle()
     },[])
 
     return (
         <div>
-            Text here
+            <h1 className="welcome-heading">WELCOME</h1>
+            <Button variant="success" className='get-started-button'>Get Started</Button>
+            <div className="image-container">
+                <img src={BusWelcomeLogo} className="welcome-image" alt="Bus and People"  />
+            </div>
         </div>
     )
 };
