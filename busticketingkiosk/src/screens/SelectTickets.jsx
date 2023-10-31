180e89f987df601ca9271153f891010312db7672
Abhay2412 <abhaykhosla0@gmail.com>
2023-10-31 11:52:14 -0600

    Added Route Map images for each and updated the route data

--- busticketingkiosk/src/screens/SelectTickets.jsx
+++ busticketingkiosk/src/screens/SelectTickets.jsx
@@ -9,25 +9,26 @@ import { TicketCountContext } from '../App';
 const SelectTicketsScreen = ({setPageTitle}) => {
     const navigate = useNavigate();
     const [selectedRoute, setSelectedRoute] = useState(null);
+    const [routeNumber, setRouteNumber] = useState("");
     const [nextDeparture, setNextDeparture] = useState('');
     const [duration, setDuration] = useState('');
     const { ticketCounts, setTicketCounts } = useContext(TicketCountContext);
     const routes = ["Bowness", "Mount Pleasant", "Sandstone", "Huntington", "Killarney", "Marda Loop", "North Pointe", "Dalhousie", "City Hall", "Castleridge", "Falconridge", "Nolan Hill"]
     const routeData = {
-        "Bowness": { departure: "10:15 AM", duration: "45 minutes" },
-        "Mount Pleasant": { departure: "10:30 AM", duration: "40 minutes" },
-        "Sandstone": { departure: "10:45 AM", duration: "55 minutes" },
-        "Huntington": { departure: "11:00 AM", duration: "50 minutes" },
-        "Killarney": { departure: "11:15 AM", duration: "60 minutes" },
-        "Marda Loop": { departure: "11:30 AM", duration: "35 minutes" },
-        "North Pointe": { departure: "11:45 AM", duration: "40 minutes" },
-        "Dalhousie": { departure: "12:00 PM", duration: "45 minutes" },
-        "City Hall": { departure: "12:15 PM", duration: "50 minutes" },
-        "Castleridge": { departure: "12:30 PM", duration: "55 minutes" },
-        "Falconridge": { departure: "12:45 PM", duration: "50 minutes" },
-        "Nolan Hill": { departure: "1:00 PM", duration: "30 minutes" }
+        "Bowness": { routeNumber: "1", nextDeparture: "10:15 AM", duration: "30 minutes", numberOfStops: 13, lastDeparture: "11:15 PM"},
+        "Mount Pleasant": { routeNumber: "2", nextDeparture: "10:30 AM", duration: "1 hour", numberOfStops: 70, lastDeparture: "11:30 PM"},
+        "Sandstone": { routeNumber: "3", nextDeparture: "10:45 AM", duration: "2 hours", numberOfStops: 80, lastDeparture: "11:15 PM" },
+        "Huntington": { routeNumber: "4", nextDeparture: "11:00 AM", duration: "2 hours 50 minutes", numberOfStops: 75, lastDeparture: "11:15 PM" },
+        "Killarney": { routeNumber: "6", nextDeparture: "11:15 AM", duration: "35 minutes", numberOfStops: 34, lastDeparture: "11:10 PM" },
+        "Marda Loop": { routeNumber: "7", nextDeparture: "11:30 AM", duration: "30 minutes", numberOfStops: 38, lastDeparture: "10:15 PM" },
+        "North Pointe": { routeNumber: "8", nextDeparture: "11:45 AM", duration: "35 minutes", numberOfStops: 42, lastDeparture: "10:20 PM" },
+        "Dalhousie": { routeNumber: "9", nextDeparture: "3:00 PM", duration: "10 minutes", numberOfStops: 5, lastDeparture: "11:10 PM" },
+        "City Hall": { routeNumber: "10", nextDeparture: "12:15 PM", duration: "1 hour 23 minutes", numberOfStops: 47, lastDeparture: "11:25 PM" },
+        "Castleridge": { routeNumber: "21", nextDeparture: "12:30 PM", duration: "30 minutes", numberOfStops: 30, lastDeparture: "11:45 PM" },
+        "Monterey Park": { routeNumber: "57", nextDeparture: "12:45 PM", duration: "28 minutes", numberOfStops: 32, lastDeparture: "11:15 PM" },
+        "Nolan Hill": { routeNumber: "82", nextDeparture: "1:00 PM", duration: "30 minutes" , numberOfStops: 35, lastDeparture: "11:00 PM"}
     };
-    const handleRouteChange = (route) => { setSelectedRoute(route); setNextDeparture(routeData[route].departure); setDuration(routeData[route].duration); };
+    const handleRouteChange = (route) => { setSelectedRoute(route); setRouteNumber(routeData[route].routeNumber); setNextDeparture(routeData[route].nextDeparture); setDuration(routeData[route].duration); };
     useEffect(() => {
         setPageTitle()
     },[]);
@@ -56,6 +57,9 @@ const SelectTicketsScreen = ({setPageTitle}) => {
                 </div>
                 <div className="mb-3 d-flex align-items-center">
                     <span className="label-text-departure mr-3"><b>Next Departure:</b> {nextDeparture}</span> 
+                </div>
+                <div className="mb-3 d-flex align-items-center">
+                    <span className="label-text-duration mr-3"><b>Route Number:</b> {routeNumber}</span> 
                 </div>
                 <div className="mb-3 d-flex align-items-center">
                     <span className="label-text-duration  mr-3"><b>Duration:</b> {duration}</span> 
--- busticketingkiosk/src/screens/routeInformation.jsx
+++ busticketingkiosk/src/screens/routeInformation.jsx
@@ -1,44 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Button, Dropdown, Container, Row, Col } from 'react-bootstrap';
-import RouteMap from "../assets/images/RouteMap.svg"
-
 
 const RouteInformation = (props) => {
     const navigate = useNavigate();
+    const [routeNumber, setRouteNumber] = useState("");
     const [nextDeparture, setNextDeparture] = useState("");
     const [duration, setDuration] = useState("");
-    const [numberOfStops, setNumberOfStops] = useState();
     const [selectedRoute, setSelectedRoute] = useState();
+    const [numberOfStops, setNumberOfStops] = useState();
     const location = useLocation();
 
     const routeData = {
-        "Bowness": { nextDeparture: "10:15 AM", duration: "45 minutes", numberOfStops: 10, lastDeparture: "11:15 PM"},
-        "Mount Pleasant": { nextDeparture: "10:30 AM", duration: "40 minutes", numberOfStops: 11, lastDeparture: "11:30 PM"},
-        "Sandstone": { nextDeparture: "10:45 AM", duration: "55 minutes", numberOfStops: 16, lastDeparture: "11:15 PM" },
-        "Huntington": { nextDeparture: "11:00 AM", duration: "50 minutes", numberOfStops: 15, lastDeparture: "11:15 PM" },
-        "Killarney": { nextDeparture: "11:15 AM", duration: "60 minutes", numberOfStops: 14, lastDeparture: "11:10 PM" },
-        "Marda Loop": { nextDeparture: "11:30 AM", duration: "35 minutes", numberOfStops: 17, lastDeparture: "10:15 PM" },
-        "North Pointe": { nextDeparture: "11:45 AM", duration: "40 minutes", numberOfStops: 12, lastDeparture: "10:20 PM" },
-        "Dalhousie": { nextDeparture: "12:00 PM", duration: "45 minutes", numberOfStops: 11, lastDeparture: "11:10 PM" },
-        "City Hall": { nextDeparture: "12:15 PM", duration: "50 minutes", numberOfStops: 15, lastDeparture: "11:25 PM" },
-        "Castleridge": { nextDeparture: "12:30 PM", duration: "55 minutes", numberOfStops: 11, lastDeparture: "11:45 PM" },
-        "Falconridge": { nextDeparture: "12:45 PM", duration: "50 minutes", numberOfStops: 13, lastDeparture: "11:15 PM" },
-        "Nolan Hill": { nextDeparture: "1:00 PM", duration: "30 minutes" , numberOfStops: 10, lastDeparture: "11:00 PM"}
+        "Bowness": { routeNumber: "1", nextDeparture: "10:15 AM", duration: "30 minutes", numberOfStops: 13, lastDeparture: "11:15 PM"},
+        "Mount Pleasant": { routeNumber: "2", nextDeparture: "10:30 AM", duration: "1 hour", numberOfStops: 70, lastDeparture: "11:30 PM"},
+        "Sandstone": { routeNumber: "3", nextDeparture: "10:45 AM", duration: "2 hours", numberOfStops: 80, lastDeparture: "11:15 PM" },
+        "Huntington": { routeNumber: "4", nextDeparture: "11:00 AM", duration: "2 hours 50 minutes", numberOfStops: 75, lastDeparture: "11:15 PM" },
+        "Killarney": { routeNumber: "6", nextDeparture: "11:15 AM", duration: "35 minutes", numberOfStops: 34, lastDeparture: "11:10 PM" },
+        "Marda Loop": { routeNumber: "7", nextDeparture: "11:30 AM", duration: "30 minutes", numberOfStops: 38, lastDeparture: "10:15 PM" },
+        "North Pointe": { routeNumber: "8", nextDeparture: "11:45 AM", duration: "35 minutes", numberOfStops: 42, lastDeparture: "10:20 PM" },
+        "Dalhousie": { routeNumber: "9", nextDeparture: "3:00 PM", duration: "10 minutes", numberOfStops: 5, lastDeparture: "11:10 PM" },
+        "City Hall": { routeNumber: "10", nextDeparture: "12:15 PM", duration: "1 hour 23 minutes", numberOfStops: 47, lastDeparture: "11:25 PM" },
+        "Castleridge": { routeNumber: "21", nextDeparture: "12:30 PM", duration: "30 minutes", numberOfStops: 30, lastDeparture: "11:45 PM" },
+        "Monterey Park": { routeNumber: "57", nextDeparture: "12:45 PM", duration: "28 minutes", numberOfStops: 32, lastDeparture: "11:15 PM" },
+        "Nolan Hill": { routeNumber: "82", nextDeparture: "1:00 PM", duration: "30 minutes" , numberOfStops: 35, lastDeparture: "11:00 PM"}
     };
 
     useEffect(() => {
-        if(location.state?.selectedRoute)
+        if(location.state?.selectedRoute && routeData[location.state.selectedRoute])
         {
             const {selectedRoute} = location.state;
             setSelectedRoute(selectedRoute);
+            setRouteNumber(routeData[selectedRoute].routeNumber);
             setNextDeparture(routeData[selectedRoute].nextDeparture);
             setDuration(routeData[selectedRoute].duration);
             setNumberOfStops(routeData[selectedRoute].numberOfStops);
         }
-    },[location.state?.route])
+    },[location.state?.selectedRoute])
 
-    const handleRouteChange = (route) => { setSelectedRoute(route); setNextDeparture(routeData[route].nextDeparture); setDuration(routeData[route].duration); setNumberOfStops(routeData[route].numberOfStops) };
+    const handleRouteChange = (route) => { setSelectedRoute(route); setRouteNumber(routeData[route].routeNumber); setNextDeparture(routeData[route].nextDeparture); setDuration(routeData[route].duration); setNumberOfStops(routeData[route].numberOfStops) };
 
     return (
         <Container className="mt-5">
@@ -61,6 +61,9 @@ const RouteInformation = (props) => {
                     </div>
                     <div style={{marginTop: "30px"}}>
                         <div className="mb-3 d-flex align-items-center">
+                            <span className="label-text-select-route"><b>Route Number:</b> {routeNumber}</span> 
+                        </div>
+                        <div className="mb-3 d-flex align-items-center">
                             <span className="label-text-select-route"><b>Next Departure:</b> {nextDeparture}</span> 
                         </div>
                         <div className="mb-3 d-flex align-items-center">
@@ -78,7 +81,7 @@ const RouteInformation = (props) => {
                     <div style={{fontFamily: "Ariel, sans-serif", fontWeight: "bold"}}>
                         Route Map
                     </div>
-                    <img src={RouteMap} alt="Route Map" />
+                    <img src={`/RouteMapImages/${selectedRoute}.png`} alt={selectedRoute ? `${selectedRoute} Route Map` : "No route selected"}  width="600px" />
                 </Col>
             </Row>
             <Row className="buttons-row">
